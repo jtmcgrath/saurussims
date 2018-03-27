19425691a10b4541ea1e9304c364ff032a9215f2
Joe McGrath <joe@beezer.com>
2018-03-27 18:52:54 +0100

    Fix infinite loop.

--- src/utils/list.js
+++ src/utils/list.js
@@ -4,19 +4,16 @@ export const generateList = (start, end) => {
 	return Array.from(Array(Math.abs(high - low) + 1), (e, i) => i + low)
 }
 
-export const generateListAroundCenter = (center, max) => {
+export const generateListAroundCenter = (origin, max) => {
+	const center = parseInt(origin, 10)
 	const pages = [center]
 
 	for (let i = 1; pages.length < 9; i++) {
-		if (center + i <= max) {
-			pages.push(center + i)
-		}
-		if (center - i > 0) {
-			pages.unshift(center - i)
-		}
-		if (pages.length >= 9) {
-			return pages
-		}
+		if (center + i <= max) pages.push(center + i)
+
+		if (center - i > 0) pages.unshift(center - i)
+
+		if (pages.length >= 9 || i >= 9) return pages
 	}
 
 	return pages
