0c4e51574402001a8c3a1b4f6c1206074c262de1
Joe McGrath <joe@beezer.com>
2018-03-29 19:25:13 +0100

    First draft of Downloads holding page.

--- src/views/customPages/Downloads.js
+++ src/views/customPages/Downloads.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import { connect as connectStyles } from 'react-fela'
 
-const Downloads = props => (
-	<pre>{JSON.stringify({ component: 'Downloads', ...props }, null, 2)}</pre>
+import { Link } from 'components'
+import { withConfig } from 'hocs'
+import { compose } from 'utils/general'
+import { listItem, listItemPadding } from 'utils/styling'
+
+const Downloads = ({ styles }) => (
+	<div className={styles.wrapper}>
+		<div className={styles.downloads}>
+			<header className={styles.header}>
+				<h1>Downloads</h1>
+				<h3>Under construction.</h3>
+			</header>
+			<p>
+				<Link routeName="tag" routeParams={{ tagName: 'downloads' }}>
+					For now, please click here to view the downloads tag instead!
+				</Link>
+			</p>
+		</div>
+	</div>
 )
 
-export default Downloads
+const styles = {
+	wrapper: () => ({
+		display: 'flex',
+		justifyContent: 'center',
+		width: '100%',
+	}),
+	downloads: ({ columnSpacing, itemPadding }) => ({
+		...listItem({ columnSpacing }),
+		...listItemPadding({ itemPadding: itemPadding * 2 }),
+		textAlign: 'center',
+		display: 'inline-block',
+	}),
+	header: ({ columnSpacing }) => ({
+		marginBottom: `${columnSpacing}px`,
+	}),
+}
+
+export default compose(
+	withConfig('columnSpacing', 'itemPadding'),
+	connectStyles(styles),
+)(Downloads)
