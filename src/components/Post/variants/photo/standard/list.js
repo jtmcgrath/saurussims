3aebd2856f41c263445e89f536c12726d159db09
Joe McGrath <joe@jmcgrath.co.uk>
2018-04-26 18:25:36 +0100

    Like on mobile. (#5)

--- src/components/Post/variants/photo/standard/list.js
+++ src/components/Post/variants/photo/standard/list.js
@@ -2,21 +2,23 @@ import React from 'react'
 import { Link } from 'react-router5'
 import classNames from 'classnames'
 
-const Standard = ({ linkProps, styles, postActions, photos, tags }) => {
+const Standard = ({ columnCount, linkProps, styles, postActions, photos, tags }) => {
 	const photoElements =
 		photos &&
 		photos.map(({ caption, thumbnail }, i) => (
 			<img key={thumbnail} src={thumbnail} alt={caption} />
 		))
 
+	const ConditionalLink = columnCount > 1 ? Link : 'div'
+
 	return (
 		<article className={classNames(styles.listItem, styles.postActions)}>
-			<Link
+			<ConditionalLink
 				{...linkProps}
 				className={classNames(linkProps.className, styles.listItemPadding)}
 			>
 				{photoElements}
-			</Link>
+			</ConditionalLink>
 			{postActions}
 		</article>
 	)
