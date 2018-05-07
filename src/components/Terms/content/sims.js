43a754c887cc616ac7bb4161e396c41a069c3d90
Joe McGrath <joe@beezer.com>
2018-05-07 20:52:41 +0100

    Change wording.

--- src/components/Terms/content/sims.js
+++ src/components/Terms/content/sims.js
@@ -6,11 +6,10 @@ const Downloads = () => (
 			<strong>My Sims</strong>
 		</p>
 		<p>
-			This is the gallery for all of my original sims. You can view posts tagged
+			This is the gallery for all of my original sim models. You can view posts tagged
 			with each sim by clicking on their photo, and if/when a sim becomes
 			available for download you can do so through this page.
 		</p>
-		<p>Don’t claim as your own, it’s rude!</p>
 	</Fragment>
 )
 
