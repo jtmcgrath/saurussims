d5bdec851725393be1a7511d94d8ceef3ce20361
Joe McGrath <joe@jmcgrath.co.uk>
2018-05-08 20:31:25 +0100

    Update sims page terms copy. (#28)

--- src/components/Terms/content/sims.js
+++ src/components/Terms/content/sims.js
@@ -6,9 +6,12 @@ const Downloads = () => (
 			<strong>My Sims</strong>
 		</p>
 		<p>
-			This is the gallery for all of my original sim models. You can view posts tagged
-			with each sim by clicking on their photo, and if/when a sim becomes
-			available for download you can do so through this page.
+			This is the gallery for all of my original sim characters/models. You can
+			view posts tagged with each sim by clicking on their photo, and if/when a
+			sim becomes available for download you can do so through this page.
+		</p>
+		<p>
+			This page is a WIP - I&apos;m working on adding a filtering system!
 		</p>
 	</Fragment>
 )
