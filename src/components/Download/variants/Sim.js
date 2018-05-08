63ad464cb6e9183f68649c849110a76e3ec06475
Joe McGrath <joe@beezer.com>
2018-05-08 20:59:18 +0100

    Add sim download link.

--- src/components/Download/variants/Sim.js
+++ src/components/Download/variants/Sim.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router5'
 import classNames from 'classnames'
 
-const Sim = ({ className, image, name, styles, tagName }) => (
-	<Link
-		className={classNames(className, styles.link)}
-		routeName="tag"
-		routeParams={{ tagName }}
-	>
-		<img className={styles.image} src={image} alt={name} />
-		<h1 className={styles.title}>{name}</h1>
-	</Link>
+import { Link, Icon } from 'components'
+
+const Sim = ({ className, download, image, name, styles, tagName }) => (
+	<div className={styles.wrapper}>
+		<Link
+			className={classNames(className, styles.link)}
+			routeName="tag"
+			routeParams={{ tagName }}
+		>
+			<img className={styles.image} src={image} alt={name} />
+			<h1 className={styles.title}>{name}</h1>
+		</Link>
+		{download && (
+			<Link className={classNames('download', styles.download)} href={download}>
+				<Icon icon="Download" />
+			</Link>
+		)}
+	</div>
 )
 
 export default Sim
