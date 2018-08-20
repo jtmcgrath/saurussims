d8c73a173a78309ec4965dce0ce667af604a54cf
Joe McGrath <joe.mcgrath@fanduel.com>
2018-08-20 08:58:04 +0100

    move router reduxPlugin init into configureRouter

--- src/dependencies/router/router.js
+++ src/dependencies/router/router.js
@@ -2,9 +2,19 @@ import createRouter from 'router5'
 import browserPlugin from 'router5/plugins/browser'
 import listenersPlugin from 'router5/plugins/listeners'
 import loggerPlugin from 'router5/plugins/logger'
+import { reduxPlugin } from 'redux-router5'
 
-export const configureRouter = ({ defaultRoute, routes }) =>
-	createRouter(routes, { defaultRoute })
+export const configureRouter = ({ defaultRoute, routes }) => {
+	const router = createRouter(routes, { defaultRoute })
 		.usePlugin(browserPlugin())
 		.usePlugin(listenersPlugin())
 		.usePlugin(loggerPlugin)
+
+	return {
+		...router,
+		start: (app, done) =>
+			router
+				.usePlugin(reduxPlugin(app.dependencies.store.dispatch))
+				.start(done),
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import { createElement } from 'react'
 import { render } from 'react-dom'
-import { reduxPlugin } from 'redux-router5'
 
 import config from './config'
 import bootstrap from './bootstrap'
 
 bootstrap(config).then(app => {
-	app.dependencies.router.usePlugin(
-		reduxPlugin(app.dependencies.store.dispatch)
-	)
-	app.dependencies.router.start(() => {
+	app.dependencies.router.start(app, () => {
 		render(
 			createElement(app.appComponent, app),
 			document.getElementById('root')
