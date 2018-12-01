41d181aaa978512ecb1f98886cb2edc54beb6d52
Joe McGrath <joe.mcgrath@fanduel.com>
2018-12-01 23:11:23 +0000

    Add Title component

--- src/tumblr/index.ts
+++ src/tumblr/index.ts
@@ -1,3 +1,4 @@
 export { default as Avatar } from './basic/Avatar'
 export { default as Description } from './basic/Description'
 export { default as MetaDescription } from './basic/MetaDescription'
+export { default as Title } from './basic/Title'
