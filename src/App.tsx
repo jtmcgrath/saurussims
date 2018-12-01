9f3fce15160287d7e99c2072e651505f5ebd6fed
Joe McGrath <joe.mcgrath@fanduel.com>
2018-12-01 23:09:51 +0000

    Add Avatar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const App = (props: {}) => <pre>{JSON.stringify(props, null, 2)}</pre>
+import { Avatar } from '~/tumblr'
+
+const App = (props: {}) => (
+	<div>
+		<Avatar />
+		<Avatar size="30" />
+		<pre>{JSON.stringify(props, null, 2)}</pre>
+	</div>
+)
 
 export default App
---
+++ src/tumblr/basic/Avatar.tsx
@@ -0,0 +1,18 @@
+import { renderString } from '~/tumblr/helpers'
+
+type AvatarSizes = '16' | '24' | '30' | '40' | '48' | '64' | '96' | '128'
+
+export interface AvatarProps {
+	size?: AvatarSizes
+}
+
+const Avatar = renderString<AvatarProps>(
+	'Avatar',
+	({ size }) => `{PortraitURL-${size}}`
+)
+
+Avatar.defaultProps = {
+	size: '128',
+}
+
+export default Avatar
