20be4ca1b45dae53eddf35d0ab2a1b8fd4937642
Joe McGrath <joe.mcgrath@fanduel.com>
2018-12-02 01:07:43 +0000

    Add CopyrightYears component

--- src/tumblr/index.ts
+++ src/tumblr/index.ts
@@ -1,5 +1,6 @@
 export { default as Avatar } from './basic/Avatar'
 export { default as BlogURL } from './basic/BlogURL'
+export { default as CopyrightYears } from './basic/CopyrightYears'
 export { default as CustomCSS } from './basic/CustomCSS'
 export { default as Description } from './basic/Description'
 export { default as FaviconURL } from './basic/FaviconURL'
