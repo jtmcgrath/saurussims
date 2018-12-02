e77d4b58d7d7c79123b8ac1a3cc046de39c56720
Joe McGrath <joe.mcgrath@fanduel.com>
2018-12-02 01:03:15 +0000

    Add BlogURL component

--- src/tumblr/index.ts
+++ src/tumblr/index.ts
@@ -1,4 +1,5 @@
 export { default as Avatar } from './basic/Avatar'
+export { default as BlogURL } from './basic/BlogURL'
 export { default as Description } from './basic/Description'
 export { default as MetaDescription } from './basic/MetaDescription'
 export { default as Title } from './basic/Title'
