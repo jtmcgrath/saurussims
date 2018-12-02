911d6c50bc8c6c80d7dd7cb88cc60f126f42b441
Joe McGrath <joe.mcgrath@fanduel.com>
2018-12-02 01:05:38 +0000

    Add FaviconURL component

--- src/tumblr/index.ts
+++ src/tumblr/index.ts
@@ -1,6 +1,7 @@
 export { default as Avatar } from './basic/Avatar'
 export { default as BlogURL } from './basic/BlogURL'
 export { default as Description } from './basic/Description'
+export { default as FaviconURL } from './basic/FaviconURL'
 export { default as MetaDescription } from './basic/MetaDescription'
 export { default as RSS } from './basic/RSS'
 export { default as Title } from './basic/Title'
