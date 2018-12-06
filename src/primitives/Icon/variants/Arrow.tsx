f0d313fbc0451620c40b3214eedbe97980ef616f
Joe McGrath <joe.mcgrath@fanduel.com>
2018-12-06 20:38:32 +0000

    Add Arrow icon

---
+++ src/primitives/Icon/variants/Arrow.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { IconProps } from '../Icon'
+
+const Arrow: React.SFC<IconProps> = props => (
+	<svg {...props}>
+		<line x1="5" y1="12" x2="19" y2="12" />
+		<polyline points="12 5 19 12 12 19" />
+	</svg>
+)
+
+export default Arrow
