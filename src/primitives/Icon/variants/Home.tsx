43ad6c0fe9f250e21d6c808dac12ed901b8c37d9
Joe McGrath <joe.mcgrath@fanduel.com>
2018-12-07 18:11:48 +0000

    Add Home icon

---
+++ src/primitives/Icon/variants/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { IconProps } from '../Icon'
+
+const Home: React.SFC<IconProps> = props => (
+	<svg {...props}>
+		<path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+		<polyline points="9 22 9 12 15 12 15 22" />
+	</svg>
+)
+
+export default Home
