284efdc68b42022c04e6e59b130a49b7bc91abc1
Joe McGrath <joe.mcgrath@fanduel.com>
2018-12-18 21:42:38 +0000

    Pass variables into createConfig

--- contentful/src/index.js
+++ contentful/src/index.js
@@ -1,12 +1,13 @@
 import buildApp from './app'
 import createConfig from './config'
+import variables from './variables'
 
 document.addEventListener('DOMContentLoaded', () => {
 	if (!window.saurussimsHasLoaded) {
 		window.saurussimsHasLoaded = true
         const root = document.querySelector('article.permalink-page')
 
-        const config = createConfig(root)
+        const config = createConfig(root, variables)
 
         if (config) {
             buildApp(root, config)
