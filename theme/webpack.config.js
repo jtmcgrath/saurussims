10ac351b88cd358a9bc993b5b762ce8c5731381c
Joe McGrath <joe.mcgrath@fanduel.com>
2019-01-06 12:44:48 +0000

    Rearrange directory structure

--- theme/webpack.config.js
+++ theme/webpack.config.js
@@ -4,16 +4,16 @@ const path = require('path')
 module.exports = {
 	entry: path.join(__dirname, 'src/index.js'),
 	output: {
-		path: path.join(__dirname, 'dist'),
+		path: path.join(__dirname, 'output'),
 		filename: 'main.js',
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: path.join(__dirname, 'dist/index.html'),
+			template: path.join(__dirname, 'output/index.html'),
 			inject: false,
 		}),
 	],
 	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
+		contentBase: path.join(__dirname, 'output'),
 	},
 }
