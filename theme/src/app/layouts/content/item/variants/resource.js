dd6938824832bf62baa02a88b3a5b86a53576d7e
Joe McGrath <joe@raptori.dev>
2020-02-24 04:04:55 +0000

    changes because tumblr is dumb af

--- theme/src/app/layouts/content/item/variants/resource.js
+++ theme/src/app/layouts/content/item/variants/resource.js
@@ -1,7 +1,7 @@
-export default function renderResource({ image, link, title, tags }) {
+export default function renderResource({ color, image, link, title, tags = [] }) {
     return `
         <div class="item resource ${tags.join(' ')}">
-            <a href="${link}" class="item-link">
+            <a href="${link}" class="item-link" ${color ? ` style="background: ${color}"` : ''}>
                 ${image ? `<img src="${image}" alt="${title}" />` : ''}
                 <h1>${title}</h1>
             </a>
